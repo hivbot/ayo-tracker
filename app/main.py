09bf4144f43bc5d1210fa4657b7b3f29c83f81d6
Andreas Martin <andreas.martin@andreasmartin.ch>
2023-08-29 19:04:27 +0200

    Add files via upload

---
+++ app/main.py
@@ -0,0 +1,76 @@
+import os
+import requests
+import logging
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import Optional
+from fastapi.responses import JSONResponse
+
+AYO_WHATSAPP_API = os.environ.get('AYO_WHATSAPP_API')
+PHONE_NUMBER_ID = os.environ.get('PHONE_NUMBER_ID')
+
+app = FastAPI()
+logging.basicConfig(level=os.getenv("LOG_LEVEL", "INFO").upper())
+logger = logging.getLogger(__name__)
+
+class BaseInput(BaseModel):
+    user_id: str
+    intent_name: str
+
+class SchedulerInput(BaseInput):
+    time_point: str
+    query_value: Optional[str] = None
+
+@app.post("/v1")
+async def post_scheduler(scheduler_input: SchedulerInput):
+    try:
+        user_id = scheduler_input.user_id
+        intent_name = scheduler_input.intent_name
+        time_point = scheduler_input.time_point
+        query_value = scheduler_input.query_value
+        
+        logger.info("Received scheduler post request:")
+        logger.info("user_id: %s", user_id)
+        logger.info("intent_name: %s", intent_name)
+        logger.info("time_point: %s", time_point)
+        logger.info("query_value: %s", query_value)
+
+        call_intent_endpoint(user_id, intent_name, query_value)
+        
+        return JSONResponse(content={"message": "Scheduler set successfully"})
+    except Exception as e:
+        logger.error("Error: %s", e)
+        raise HTTPException(status_code=500, detail="Internal Server Error")
+
+@app.delete("/v1")
+async def delete_scheduler(delete_input: BaseInput):
+    try:
+        user_id = delete_input.user_id
+        intent_name = delete_input.intent_name
+        
+        logger.info("Received scheduler delete request:")
+        logger.info("user_id: %s", user_id)
+        logger.info("intent_name: %s", intent_name)
+        
+        return JSONResponse(content={"message": "Scheduler deleted successfully"})
+    except Exception as e:
+        logger.error("Error: %s", e)
+        raise HTTPException(status_code=500, detail="Internal Server Error")
+
+def call_intent_endpoint(user_id, intent_name, query_value=""):
+    try:
+        payload = {
+            'user_id': user_id,
+            'intent_name': intent_name,
+            'query_value': query_value,
+            'phone_number_id': PHONE_NUMBER_ID,
+            'user_name': "Ayo Scheduler"
+        }
+        response = requests.post(AYO_WHATSAPP_API + '/intent', json=payload)
+        if response.status_code == 200:
+            logger.info('Request successful')
+        else:
+            logger.error('Request failed with status code: %d', response.status_code)
+        
+    except Exception as e:
+        logger.error('Error: %s', e)
